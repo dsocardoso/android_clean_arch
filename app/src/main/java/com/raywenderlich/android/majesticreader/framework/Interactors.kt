cd0c3cc421f472f64877bfa43184965ffe0397ee
Diego Cardoso <diego.cardoso@mercadolivre.com>
2022-04-11 16:09:00 -0300

    added last files and finished

--- app/src/main/java/com/raywenderlich/android/majesticreader/framework/Interactors.kt
+++ app/src/main/java/com/raywenderlich/android/majesticreader/framework/Interactors.kt
@@ -1,3 +1,21 @@
 package com.raywenderlich.android.majesticreader.framework
 
-class Interactors()
+import com.raywenderlich.android.majesticreader.interactors.AddBookmark
+import com.raywenderlich.android.majesticreader.interactors.AddDocument
+import com.raywenderlich.android.majesticreader.interactors.GetBookmarks
+import com.raywenderlich.android.majesticreader.interactors.GetDocuments
+import com.raywenderlich.android.majesticreader.interactors.GetOpenDocument
+import com.raywenderlich.android.majesticreader.interactors.RemoveBookmark
+import com.raywenderlich.android.majesticreader.interactors.RemoveDocument
+import com.raywenderlich.android.majesticreader.interactors.SetOpenDocument
+
+class Interactors(
+    val addBookmark: AddBookmark,
+    val getBookmarks: GetBookmarks,
+    val deleteBookmark: RemoveBookmark,
+    val addDocument: AddDocument,
+    val getDocuments: GetDocuments,
+    val removeDocument: RemoveDocument,
+    val getOpenDocument: GetOpenDocument,
+    val setOpenDocument: SetOpenDocument
+)
